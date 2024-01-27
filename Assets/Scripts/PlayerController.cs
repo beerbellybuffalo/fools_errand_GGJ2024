70ff5705d5570da58aa0d8abaa88a783c20d3f1f
Daniel Tang <tangjuqian@yahoo.com.sg>
2024-01-27 20:13:49 +0800

    Rigging done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D rb;
+    public float jumpForce;
+    public float playerSpeed;
+    public Vector2 jumpHeight;
+    private bool isOnGround;
+    public float positionRadius;
+    public LayerMask ground;
+    public Transform playerPos;
+    void Start()
+    {
+        Collider2D[] colliders = transform.GetComponentsInChildren<Collider2D>();
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            for (int k = i+1; k < colliders.Length; k++)
+            {
+                Physics2D.IgnoreCollision(colliders[i], colliders[k]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
